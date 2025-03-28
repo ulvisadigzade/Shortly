c1e1e99d6ae04f600109c6d387096b7df0ad3907
Ulvi Sadigzade <ulvisadiqzade05@gmail.com>
2025-03-28 17:51:42 +0400

    UrlGenerator fix

--- src/main/java/com/shortly/shortly/util/UrlGenerator.java
+++ src/main/java/com/shortly/shortly/util/UrlGenerator.java
@@ -3,6 +3,7 @@ package com.shortly.shortly.util;
 import com.shortly.shortly.repository.ShortenerRepository;
 import org.springframework.stereotype.Component;
 
+import java.security.SecureRandom;
 import java.util.Random;
 
 @Component
@@ -11,20 +12,22 @@ public class UrlGenerator {
     private final String alphaNumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     private final int length = 6;
     private final int alphaNumericLength = alphaNumeric.length();
-    Random rand = new Random();
+    Random rand = new SecureRandom();
 
     public UrlGenerator(ShortenerRepository shortenerRepository){
         this.shortenerRepository=shortenerRepository;
     }
 
     public String generateShortUrl(){
-        StringBuffer newUrl = new StringBuffer("AAAAAA");
+        StringBuilder newUrl = new StringBuilder(length);
         do {
             for(int i=0;i<length;i++){
                 int randomValue = rand.nextInt(alphaNumericLength);
                 newUrl.setCharAt(i,alphaNumeric.charAt(randomValue));
             }
+
         }while(shortenerRepository.existsByShortUrl(newUrl.toString()));
+
         return newUrl.toString();
     }
 }
